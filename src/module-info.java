221bd10507dd22570c3ede4da3ab514c4f51d6ab
Ashley <ashley@foxgenesis.net>
2023-01-16 15:30:19 -0600

    updated to v1.0.0

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author 
+ *
+ * @provides net.foxgenesis.watame.plugin.IPlugin
+ */
+module pluginTemplate {
+	requires watamebot;
+
+	provides net.foxgenesis.watame.plugin.IPlugin with my.plugin.PluginTemplate;
+}
---
+++ src/my/plugin/PluginTemplate.java
@@ -0,0 +1,46 @@
+package my.plugin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.foxgenesis.watame.WatameBot;
+import net.foxgenesis.watame.WatameBot.ProtectedJDABuilder;
+import net.foxgenesis.watame.plugin.IPlugin;
+import net.foxgenesis.watame.plugin.PluginProperties;
+
+/**
+ * @author
+ *
+ */
+@PluginProperties(name = "", description = "", version = "0.0.0", providesCommands = false)
+public class PluginTemplate implements IPlugin {
+	/**
+	 * Logger
+	 */
+	private static final Logger logger = LoggerFactory.getLogger("PluginTemplate");
+	
+	@Override
+	public void preInit() { 
+		// TODO Auto-generated method stub
+	 }
+
+	@Override
+	public void init(ProtectedJDABuilder builder) { 
+		// TODO Auto-generated method stub
+	 }
+
+	@Override
+	public void postInit(WatameBot bot) { 
+		// TODO Auto-generated method stub
+	 }
+
+	@Override
+	public void onReady(WatameBot bot) { 
+		// TODO Auto-generated method stub
+	 }
+	
+	@Override
+	public void close() throws Exception { 
+		// TODO Auto-generated method stub
+	 }
+}
