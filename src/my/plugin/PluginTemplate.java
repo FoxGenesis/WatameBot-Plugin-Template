eb424acb3bd83f386ba7c564f873d8fbd44351d9
Ashley <ashley@foxgenesis.net>
2023-08-20 19:40:31 -0500

    1.1.2

--- src/my/plugin/PluginTemplate.java
+++ src/my/plugin/PluginTemplate.java
@@ -1,48 +1,69 @@
 package my.plugin;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 
 import net.foxgenesis.watame.WatameBot;
 import net.foxgenesis.watame.plugin.CommandProvider;
 import net.foxgenesis.watame.plugin.IEventStore;
 import net.foxgenesis.watame.plugin.Plugin;
-import net.foxgenesis.watame.plugin.SeverePluginException;
-
-import org.apache.commons.configuration2.Configuration;
 
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
 /**
+ * Template outlining typical plugin setup.
+ * 
  * @author Ashley
- *
  */
 public class PluginTemplate extends Plugin implements CommandProvider {
 
-	@Override
-	public void preInit() {}
+	/**
+	 * No argument constructor specified by
+	 * {@link java.util.ServiceLoader.Provider#get Provider.get()}. If you wish to
+	 * have a custom constructor, declare a {@code public static Plugin provider()}
+	 * method.
+	 */
+	public PluginTemplate() {
+		super();
+		// Load configuration settings or initialize constants
+	}
 
 	@Override
-	protected void init(IEventStore eventStore) throws SeverePluginException {}
+	public void preInit() {
+		// Load required resources
+		// Register custom databases
+	}
 
 	@Override
-	public void postInit(WatameBot bot) {}
+	protected void init(IEventStore eventStore) {
+		// Register/retrieve plugin properties
+		// Load main chunk of program
+		// Register event listeners
+	}
 
 	@Override
-	public void onReady(WatameBot bot) {}
+	public void postInit(WatameBot bot) {
+		// Initialization cleanup
+		// Unload unneeded resources
+	}
+
+	@Override
+	public void onReady(WatameBot bot) {
+		// Called after all plugins have finished post initialization and we are
+		// connected to discord
+	}
 
 	@Override
 	public void close() throws Exception {
-
+		// Unload/shutdown all open resources
+		// Databases and event listeners will be unloaded automatically
 	}
 
 	@Override
 	public Collection<CommandData> getCommands() {
+		// Register commands
+		// If there are no commands to register, remove CommandProvider from
+		// plugin declaration
 		return Set.of();
 	}
-
-	@Override
-	protected void onConstruct(Properties meta, Map<String, Configuration> configs) {}
 }
